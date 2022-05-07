589050cf5b5edf4c18efe5ff6fdf9edebb82206c
Destiny <dstny_smmns@yahoo.com>
2022-05-07 18:15:36 -0400

    First commit

---
+++ src/posproj/Db.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posproj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author dstny
+ */
+public class Db {
+    
+    public static Connection mycon(){
+    
+        Connection con = null;
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/logindatabase","root","");
+            
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+        return con;
+}
+    
+}
